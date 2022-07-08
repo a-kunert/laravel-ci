541c86878d887c44c228b409d26fc92bb2a3664b
Aaron Kunert <aaron.kunert@abiturma.de>
2022-07-08 12:07:21 +0200

    no db migrations

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -2,14 +2,13 @@
 
 namespace Tests\Browser;
 
-use App\Models\UserType;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class ExampleTest extends DuskTestCase
 {
-    use DatabaseMigrations;
+//    use DatabaseMigrations;
 
     /**
      * A basic browser test example.
